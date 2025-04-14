fa259b902ec9aed1b7a403bf68b0bf63ee89cc3f
Gerald <geraldbaalham@live.co.uk>
2025-04-14 19:48:45 +0100

    refactor: remove unused assets and styles, update App component structure

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <main>
+      <div>
+        <h1>Portfolio</h1>
+        <p>Welcome to my portfolio website!</p>
+      </div>
+    </main>
+  );
+};
+export default App;
